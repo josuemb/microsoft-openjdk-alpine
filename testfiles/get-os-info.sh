059618a14652ae1842d3fff5728ee2b3937517c2
Josué Martínez Buenrrostro <josuemb@users.noreply.github.com>
2022-05-22 18:17:16 -0500

    Update get-os-info.sh

--- testfiles/get-os-info.sh
+++ testfiles/get-os-info.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-cat /etc/os-release | sed 's|"||g' | awk -F '=' 'BEGIN{print "["} NR>1 {print ","} {print "\""$1"\""":""\""$2"\""} END{print "]"}' | paste -d "\0" -s
+cat /etc/os-release | sed 's|"||g' | awk -F '=' 'BEGIN{print "{"} NR>1 {print ","} {print "\""$1"\""":""\""$2"\""} END{print "}"}' | paste -d "\0" -s
